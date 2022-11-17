7dc62430a7f86bfbe79b2adb264400ef89921aa7
paintingofblue <micgar19@kingshigh.school.nz>
2022-11-17 21:59:56 +1300

    Add styles class and refactor some code

--- main.py
+++ main.py
@@ -1,8 +1,31 @@
 import sys
-import requests
 import PyQt5.QtWidgets as Qt
-import qdarkstyle
-from PyQt5.QtGui import QIcon
+import PyQt5.QtGui as QtGui
+
+class Styles:
+    dark = """
+        * {
+            background-color: #2A3132;
+            color: #F0F0F0;
+        }
+        
+        QTextEdit {
+            background-color: #3A4142;
+            color: #F0F0F0;
+    }"""
+
+    light = """
+    * {
+        background-color: #F0F0F0;
+        color: #000000;
+    }
+
+    QTextEdit {
+        background-color: #FFFFFF;
+        color: #000000;
+    }"""
+
+    isDarkMode = False
 
 sites = [
     "Instagram",
@@ -15,35 +38,30 @@ sites = [
     "Twitch",
 ]
 
-def dark_mode(darkMode):
-    if darkMode.isChecked():
-        app.setStyleSheet(qdarkstyle.load_stylesheet_pyqt5())
-
-        optionsButton.setIcon(QIcon("icons/Settings-Dark.png"))
-        startButton.setIcon(QIcon("icons/Play-Dark.png"))
-
-        wordlistLabel.move(15, 420)
-        siteLabel.move(218, 420)
+def dark_mode(darkmode):
+    global isDarkMode
+    if darkmode.isChecked():
+        app.setStyleSheet(Styles.dark)
+        optionsButton.setIcon(QtGui.QIcon("icons/Settings-Dark.png"))
+        startButton.setIcon(QtGui.QIcon("icons/Play-Dark.png"))
+        Styles.isDarkMode = True
     else:
-        app.setStyleSheet("")
-
-        optionsButton.setIcon(QIcon("icons/Settings-Light.png"))
-        startButton.setIcon(QIcon("icons/Play-Light.png"))
-        
-        wordlistLabel.move(20, 420)
-        siteLabel.move(223, 420)
+        app.setStyleSheet(Styles.light)
+        optionsButton.setIcon(QtGui.QIcon("icons/Settings-Light.png"))
+        startButton.setIcon(QtGui.QIcon("icons/Play-Light.png"))
+        Styles.isDarkMode = False
 
 def options():
-    e = Qt.QWidget()
-    e.setWindowTitle("Options")
-    e.setWindowIcon(QIcon("icon.png"))
-    e.setFixedSize(300, 200)
-    e.show()
+    optionsWindow = Qt.QWidget()
+    optionsWindow.setWindowTitle("Options")
+    optionsWindow.setWindowIcon(QtGui.QIcon("icon.png"))
+    optionsWindow.setFixedSize(300, 200)
+    optionsWindow.show()
 
     # Dark mode toggle
-    darkMode = Qt.QCheckBox("Dark Mode", e)
+    darkMode = Qt.QCheckBox("Dark Mode", optionsWindow)
     darkMode.move(10, 10)
-    darkMode.setChecked(False)
+    darkMode.setChecked(Styles.isDarkMode)
     darkMode.stateChanged.connect(lambda: dark_mode(darkMode))
     darkMode.show()
 
@@ -54,17 +72,18 @@ def options():
             break
 
 def wordlist_open():
-    file , check = Qt.QFileDialog.getOpenFileName(None, "QFileDialog.getOpenFileName()", "", "Text Files (*.txt)")
+    file, check = Qt.QFileDialog.getOpenFileName(None, "QFileDialog.getOpenFileName()", "", "Text Files (*.txt)")
     if check:
         wordlistPath.setText(file)
 
 if __name__ == "__main__":
     # Creating the window
     app = Qt.QApplication(sys.argv)
+    app.setStyle('Fusion')
+
     w = Qt.QWidget()
     w.setFixedSize(640, 480)
     w.setWindowTitle("Username Checker")
-
 
     # Wordlist button & path
     wordlistLabel = Qt.QLabel(w)
@@ -101,7 +120,7 @@ if __name__ == "__main__":
 
     # Options button
     optionsButton = Qt.QPushButton(w)
-    optionsButton.setIcon(QIcon("icons/Settings-Light.png"))
+    optionsButton.setIcon(QtGui.QIcon("icons/Settings-Light.png"))
     optionsButton.resize(50, 22)
     optionsButton.move(503, 440)
     optionsButton.clicked.connect(options)
@@ -109,7 +128,7 @@ if __name__ == "__main__":
 
     # Start button
     startButton = Qt.QPushButton(w)
-    startButton.setIcon(QIcon("icons/Play-Light.png"))
+    startButton.setIcon(QtGui.QIcon("icons/Play-Light.png"))
     startButton.resize(50, 22)
     startButton.move(571, 440)
     startButton.show()
@@ -120,6 +139,6 @@ if __name__ == "__main__":
     text_box.setFixedSize(600, 385)
     text_box.move(20, 20)
     text_box.show()
-    
+
     w.show()
     sys.exit(app.exec_())
